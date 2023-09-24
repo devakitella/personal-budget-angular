3742e4092183651929016f856ede868f850f7314
devakitella <devakitella@gmail.com>
2023-09-23 21:47:49 -0400

    Added Angular

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,49 @@
+import { AfterViewInit, Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import Chart from 'chart.js/auto';
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements AfterViewInit {
+
+  public data:any[] = []
+  public labels:any[] = []
+  public dataSource = {
+        datasets: [
+            {
+                data: this.data,
+                backgroundColor: [
+                    '#ffcd56',
+                    '#ff6384',
+                    '#36a2eb',
+                    '#fd6b19',
+                ]
+            }
+        ],
+        labels: this.labels
+  }
+
+  createChart() {
+      var ctx = document.getElementById('myChart') as HTMLCanvasElement;
+      var myPieChart = new Chart(ctx, {
+          type: "pie",
+          data: this.dataSource
+      });
+  }
+
+  constructor(private http: HttpClient){  }
+
+  ngAfterViewInit(): void {
+      this.http.get('http://localhost:3000/budget')
+      .subscribe((res: any) => {
+        for (var i = 0; i < res.myBudget.length; i++) {
+          this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+          this.dataSource.labels[i] = res.myBudget[i].title;
+        }
+        this.createChart();
+      })
+  }
+}
